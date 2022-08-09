0bef83361fad2d0b7a15b55c6384135e4cbb0378
Gabriel Oliveira <gabrieloliveira.okada@gmail.com>
2022-08-08 23:45:52 -0300

    Respondido

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,21 @@
-const num = //seu numero aqui
+const num = Number(prompt("Insira um número aqui:"))
+//1. utilizando ifs aninhados
 
+/* if(num % 2 ===0){
+    console.log(num, "É divisível por 2.")
+    if(num %3 ===0){
+       console.log(num, "é divisível por 3.")
+    }
+}else console.log("Toma vergonha na cara e coloca o número direito!")*/
+
+if(num%2 ===0 && num %3 ===0){
+    console.log(num, "É divisível por 2 e 3.")    
+}else{
+    console.log(num,"Não é divisível por 2 e 3.")
+}
+// verifique se um número é divisível por 2 ou 3.
+
+//ESTUDAR OPERADOR RELACIONAL. "E, OU"
+// E=&&     OU=|| 
+
+//2 - Utilizando um operador lógico para unir duas operações relacionais
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,4 +1,6 @@
 //código a ser reescrito
+const nome = prompt('Qual é seu nome?')
+const idade = Number(prompt("Qual é a sua idade?"))
 
 if(nome === "José"){
 	console.log("Oi, Zé!");
@@ -11,3 +13,9 @@ if(idade >= 18){
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
 }
+
+//reescrever o código usando o IF TERNÁRIO.
+
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá",+nome)
+
+idade >=18 ? console.log("Pode tirar carteira de motorista!") : console.log("Não pode tirar carteira de motorista")
